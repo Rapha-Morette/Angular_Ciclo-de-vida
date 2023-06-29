cc6dc8d094b13848daf403bbb4ba150daf7869ad
Rapha-Morette <raphaelmrtt14@gmail.com>
2023-06-29 16:17:59 -0300

    módulo 2 video 2 concluído

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-
+  valorItem!: string;
   constructor() { }
 
   ngOnInit(): void { }
+
+  adicionarItem() {
+    console.log(this.valorItem);
+  }
 }
